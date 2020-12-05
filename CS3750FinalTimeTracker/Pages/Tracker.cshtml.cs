4ae06508f712f0832ec0679812c6617fd697935e
madisonjones515@gmail.com <madisonjones515@gmail.com>
2020-12-05 12:04:41 -0700

    Added files

---
+++ CS3750FinalTimeTracker/Pages/Tracker.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS3750FinalTimeTracker.Pages
+{
+    public class TrackerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
